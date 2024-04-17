1edaba4bf1cce0fc27db5fd656911b4406f1388d
nvrn <70858920+nvrn@users.noreply.github.com>
2024-04-17 21:31:40 +0300

    Start backend

---
+++ index2.php
@@ -0,0 +1,13 @@
+<?php 
+
+class user {
+    public $username = 'Dimqa'
+    public $usermail = 'dev.nvrn@ya.ru'
+}
+
+$dima = new user();
+
+$sofi = new user();
+
+echo 'username: ' .$dima->username . '<br>';
+echo 'usermail: ' .$dima->usermail . '<br>';
---
+++ theme/templates/home-page.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <header class="header bg-danger text-white pt-4 pb-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    Logo
+                </div>
+                <div class="col-4">
+                    Menu
+                </div>
+                <div class="col-4">
+                    Actions
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="hero pt-5 pb-5">
+        <div class="container">
+            Slider content
+        </div>
+    </div>
+    <div class="related pt-3 pb-3">
+        <div class="container">
+            <h3>Related</h3>
+        </div>
+        <div class="container">
+            Related products
+        </div>
+    </div>
+    <div class="sales pt-3 pb-3">
+        <div class="container">
+            <h3>Sales</h3>
+        </div>
+        <div class="container">
+            Sales products
+        </div>
+    </div>
+    <div class="categories pt-3 pb-3">
+        <div class="container">
+            <h3>Categories</h3>
+        </div>
+        <div class="container">
+            Products categories
+        </div>
+    </div>
+    <div class="about pt-3 pb-3">
+        <div class="container">
+            <h3>About company</h3>
+        </div>
+        <div class="container">
+            About information
+        </div>
+    </div>
+    <div class="feedback pt-3 pb-3">
+        <div class="container">
+            <h3>Feedback me</h3>
+        </div>
+        <div class="container">
+            Feedback form
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="container pt-4 pb-4">
+            <div class="row">
+                <div class="col-4">
+                    Logo
+                </div>
+                <div class="col-8">
+                    Menu
+                </div>
+            </div>
+        </div>
+        <div class="bg-danger text-white pt-2 pb-2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        Policy
+                    </div>
+                    <div class="col-6">
+                        Copyright
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
