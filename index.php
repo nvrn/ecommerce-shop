552dd300972f7c4210d6999a13b7c7139819014f
nvrn <70858920+nvrn@users.noreply.github.com>
2024-04-17 21:18:40 +0300

    Add Bootstrap styles

--- index.php
+++ index.php
@@ -7,6 +7,97 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    Check deploy to host
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    Logo
+                </div>
+                <div class="col-4">
+                    Menu
+                </div>
+                <div class="col-4">
+                    Actions
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="hero">
+        <div class="conatiner">
+            Slider content
+        </div>
+    </div>
+    <div class="related">
+        <div class="container">
+            <h3>Related</h3>
+        </div>
+        <div class="container">
+            <div class="row">
+                Related products
+            </div>
+        </div>
+    </div>
+    <div class="sales">
+        <div class="container">
+            <h3>Sales</h3>
+        </div>
+        <div class="container">
+            <div class="row">
+                Sales products
+            </div>
+        </div>
+    </div>
+    <div class="categories">
+        <div class="container">
+            <h3>Categories</h3>
+        </div>
+        <div class="container">
+            <div class="row">
+                Products categories
+            </div>
+        </div>
+    </div>
+    <div class="about">
+        <div class="container">
+            <h3>About company</h3>
+        </div>
+        <div class="container">
+            <div class="row">
+                About information
+            </div>
+        </div>
+    </div>
+    <div class="feedback">
+        <div class="container">
+            <h3>Feedback me</h3>
+        </div>
+        <div class="container">
+            <div class="row">
+                Feedback form
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    Logo
+                </div>
+                <div class="col-8">
+                    Menu
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    Policy
+                </div>
+                <div class="col-6">
+                    Copyright
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
