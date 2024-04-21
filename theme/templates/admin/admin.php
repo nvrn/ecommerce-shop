2c5b2e438a7c6ca358abed47f81fa5522b126137
nvrn <70858920+nvrn@users.noreply.github.com>
2024-04-21 17:01:36 +0300

    Add admin page

---
+++ theme/templates/admin/admin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Админка</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <header class="admin-bar bg-dark">
+        <div class="container">
+            <img src="" alt="" class="admin-bar__logo float-left">
+            <div class="admin-bar__actions user-actions float-right">
+                <div class="user-actions__item">
+                    user@login.ru
+                </div>
+                <button class="user-actions__item">
+                    Выйти
+                </button>
+            </div>
+        </div>
+    </header>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
+</body>
+</html>
